ebf340a67a349ec48796b5e72275d355dddcac66
qiperman <matveevvitya7@yandex.ru>
2018-03-28 14:35:01 +0300

    Add EventLogReader
    Reads Event Log from Event Log Name and Print information about the number of events for each type of event (EventType: Error, Warning, Information, etc.) and the time of the last event of each type.

--- Program.cs
+++ Program.cs
@@ -35,7 +35,6 @@ namespace WinEventLogReader
             do
             {
                 menuResult = menu.PrintMenu();
-
                 //Выполняем действие меню
                 methods[menuResult]();
                 Console.ReadKey();
@@ -61,7 +60,7 @@ namespace WinEventLogReader
     class EventLogMenu
     {
         static IEnumerable<string> items { get; set; } = EventLog.GetEventLogs().Select(n => n.Log);
-        static ConsoleMenu menu { get; set; } = menu = new ConsoleMenu(items);
+        static ConsoleMenu menu { get; set; } = new ConsoleMenu(items);
 
         //Выводим список журналов как пункты меню
         static public void PrintEventMenu()
@@ -71,7 +70,12 @@ namespace WinEventLogReader
             {
                 menuResult = menu.PrintMenu();
                 EventChangeLog(items.ToArray()[menuResult]);
-                ApplicationMainMenu.Print();
+                Console.WriteLine("Нажмите Enter для выхода в главное меню");
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    ApplicationMainMenu.Print();
+                }
+
             } while (true);
         }
 
@@ -79,10 +83,79 @@ namespace WinEventLogReader
         static void EventChangeLog(string LogName)
         {
             Console.Clear();
-            Console.WriteLine($"Вы выбрали {LogName}");
-            Console.ReadKey();
+            EventLogReader eventLogReader = new EventLogReader(LogName);
+            eventLogReader.PrintDayEvents();
 
         }
+    }
+
+    class EventLogReader
+    {
+        private EventLog eventLog { get; }  //объект,который обеспечивает взаимодействие с журналами событий Windows.
+
+        public EventLogReader(string EventLogName)
+        {
+            eventLog = new EventLog();
+            ChangeLog(EventLogName);
+
+        }
+
+        //Метод вывода на консоль, событий произошедших за сутки
+        public void PrintDayEvents()
+        {
+            try
+            {
+                //Создаем массив и копируем туда Entries, для того чтобы работать с Linq
+                EventLogEntry[] EventLogEntryArray = new EventLogEntry[eventLog.Entries.Count];
+                eventLog.Entries.CopyTo(EventLogEntryArray, 0);
+
+                //Выводим в консоль события за последние сутки(Время сейчас - 24 часа)
+                Print(EventLogEntryArray.Where(d => d.TimeWritten >= DateTime.Now.AddHours(-24)));
+            }
+            catch
+            {
+                Console.WriteLine("Произошла ошибка");
+            }
+
+        }
+
+        //Метод смены журнала
+        public void ChangeLog(string EventLogName)
+        {
+            //Проверка на существование журнала в системе 
+            if (!EventLog.SourceExists(EventLogName))
+            {
+                //Журнала нет
+                Console.WriteLine("Журнала нет");
+                //Обнуляем журнал, для того чтобы при выводе на консоль не выводился старый журнал
+                eventLog.Log = "";
+            }
+            else
+            {
+                //Создаем объект типа EventLog с журналом EventLogName
+                eventLog.Log = EventLogName;
+            }
+        }
+
+        //Метод вывода на экран событий, переданных по параметру
+        private void Print(IEnumerable<EventLogEntry> EventLogEntryDayArray)
+        {
+            Console.WriteLine($"Название журнала: {eventLog.Log}"); //Вывод названия журнала
+            Console.WriteLine($"Количество записей:\n  Всего: {eventLog.Entries.Count}");//Вывод количества записей, всего и за последние сутки
+            Console.WriteLine($"  За последние сутки({DateTime.Now.AddHours(-24)}-{DateTime.Now}): {EventLogEntryDayArray.Count()}\n");
+
+            Console.WriteLine($" {"Тип записи",-15} | {"Количество",10} | {"Последняя запись"}");//Вывод заголовков таблицы таблицы
+            Console.WriteLine(" ---------------------------------------------------");
+
+            //Цикл по уникальный значениям типа события 
+            foreach (EventLogEntryType ev in EventLogEntryDayArray.Select(t => t.EntryType).Distinct())
+            {
+                //Вывод типа события, количества, и время последней записи
+                Console.WriteLine($" {ev,-15} | {EventLogEntryDayArray.Where(t => t.EntryType == ev).Count(),10} | {EventLogEntryDayArray.Where(t => t.EntryType == ev).Last().TimeWritten}");
+            }
+            Console.WriteLine("\n");
+        }
+
     }
 
     //Класс меню
@@ -129,6 +202,8 @@ namespace WinEventLogReader
                 }
             }
             while (key.Key != ConsoleKey.Enter);
+
+
             return counter;
         }
 
